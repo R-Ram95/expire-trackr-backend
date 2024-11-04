ca47887fc6a4028127b1daf288f57e5a7f6ef1f1
R-Ram95 <rohinesh@ualberta.ca>
2024-11-04 11:20:37 -0700

    prisma init

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,30 @@
 import express from "express"
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-	res.send("Bye, world");
+
+app.post("/user", async (req, res) => {
+	const user = await prisma.user.create({
+		data: {
+			name: 'Alice',
+			email: 'alice@prisma.io',
+		},
+	})
+
+	console.log(user)
+}
+);
+
+app.get("/user", async (req, res) => {
+	const user = await prisma.user.findMany();
+
+	res.send(user);
 });
+
 
 app.listen(port, () => {
 	console.log(`Server running on port ${port}`);
