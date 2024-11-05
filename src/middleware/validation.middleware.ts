666aa185ddb3b0a524fdae48846b25cc8d68670e
R-Ram95 <rohinesh@ualberta.ca>
2024-11-05 11:24:51 -0700

    added zod for schema validation

---
+++ src/middleware/validation.middleware.ts
@@ -0,0 +1,35 @@
+import { z, ZodError, ZodIssue } from "zod";
+import { Request, Response, NextFunction } from "express";
+import { ReasonPhrases, StatusCodes } from "http-status-codes";
+
+export const validateSchema = (schema: z.ZodObject<any, any>) => {
+	return (req: Request, res: Response, next: NextFunction) => {
+
+		try {
+
+			schema.parse(req);
+			next();
+
+		} catch (error) {
+
+			if (error instanceof ZodError) {
+				const errorMessages = error.errors.map((issue: ZodIssue) => {
+					return {
+						message: `Field ${issue.path} is ${issue.message}`,
+					};
+				});
+
+				res.status(StatusCodes.BAD_REQUEST).send({
+					message: ReasonPhrases.BAD_REQUEST,
+					details: errorMessages,
+				});
+
+				// Other errors
+			} else {
+				res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
+					message: ReasonPhrases.INTERNAL_SERVER_ERROR,
+				});
+			}
+		}
+	};
+};
