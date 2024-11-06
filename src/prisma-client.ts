eb5c134cbeb033e5cb9274dabe8eb02249ac4417
R-Ram95 <rohinesh@ualberta.ca>
2024-11-05 18:11:30 -0700

    added logging to prisma

--- src/prisma-client.ts
+++ src/prisma-client.ts
@@ -1,7 +1,31 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient({
 	errorFormat: "pretty",
+	log: [
+		{ emit: "event", level: "query" },
+		{
+			emit: "event",
+			level: "error",
+		},
+	],
+});
+
+// log on query
+prisma.$on("query", (e: Prisma.QueryEvent) => {
+	console.log("");
+	console.log("Query:", e.query);
+	console.log("Params:", e.params);
+	console.log("Target:", e.target);
+	console.log("Duration:", e.duration);
+	console.log("");
+});
+
+// log on error
+prisma.$on("error", (e: Prisma.LogEvent) => {
+	console.log("Message:", e.message);
+	console.log("Target:", e.target);
+	console.log("Timestamp:", e.timestamp);
 });
 
 export default prisma;
